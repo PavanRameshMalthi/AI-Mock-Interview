4cfe01876c453e5dd21892b1162634efc40475ed
PavanRameshMalthi <pavanrameshmalthi886@gmail.com>
2026-06-16 19:45:59 +0530

    Add authentication routes for user registration and login

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -10,6 +11,9 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("🚀 AI Mock Interview API Running...");
